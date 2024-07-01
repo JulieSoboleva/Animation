388f3c18d85c90facf0f4fda646e0167208824dc
JulieSoboleva <ju.soboll@gmail.com>
2024-07-01 14:26:34 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import Widget from "./widget/widget";
+
+export default function app() {
+  const widget = new Widget();
+  widget.render();
+}
+
+app();
---
+++ src/js/widget/widget.js
@@ -0,0 +1,53 @@
+import { container } from './template';
+
+export default class Widget {
+  constructor() {
+    this.units = [];
+  }
+
+  render() {
+    document.body.append(container());
+
+    this.collapsible = document.querySelector('.collapsible');
+
+    document.querySelector('.btn').addEventListener(
+      'click',
+      (event) => {
+        event.preventDefault();
+        this.toggleElement(this.collapsible);
+      },
+      false
+    );
+
+    document.addEventListener(
+      'click',
+      (event) => {
+        event.preventDefault();
+        if (event.target !== document.querySelector('.btn')) {
+          try {
+            this.closeElement();
+          } catch (e) {
+            console.error(e);
+          }
+        }
+      },
+      false
+    );
+  }
+
+  toggleElement(element) {
+    if (!element.style.height || element.style.height === '0px') {
+      element.style.height = `${Array.prototype.reduce.call(
+        element.childNodes,
+        (p, c) => p + (c.offsetHeight || 0),
+        0
+      )}px`;
+    } else {
+      element.style.height = '0px';
+    }
+  }
+
+  closeElement() {
+    this.collapsible.style.height = '0px';
+  }
+}
